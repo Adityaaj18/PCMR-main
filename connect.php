ad7589f189748126c419a2aa4fd3f75bbfea8774
anishpimple <31899357+anishpimple@users.noreply.github.com>
2020-12-05 21:04:50 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -11,9 +11,10 @@ if(mysqli_connect_errno($dbconnect))
     echo "failed to connect";
 }
 else{
-	echo"connection successful";
+	
 	$sql="INSERT INTO `cust_reg`(`username`, `email`, `phone`, `cust_msg`) VALUES ('$username','$email','$phone','$message')";
 	$query_run=mysqli_query($dbconnect,$sql);
+	echo '<script>alert("Registered Successfully")</script>'; 
 	
 }
 
